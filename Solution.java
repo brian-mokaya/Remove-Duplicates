203682543462c1ef2791b287debd8edbed20d1e1
mokaya <Brianmokaya507@gmail.com>
2026-05-19 00:46:13 +0300

    Implement removeDuplicates method in Solution class

--- Solution.java
+++ Solution.java
@@ -1,1 +1,12 @@
-
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int x=0;
+        for(int i=1;i<nums.length;i++){
+            if(nums[x]!=nums[i]){
+                x++;
+                nums[x]=nums[i];
+            }
+        }
+        return x+1;
+    }
+}
